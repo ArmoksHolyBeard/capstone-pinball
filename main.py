fdac21ce24d820c753ef5f1656397d3d82a35e10
ArmoksHolyBeard <ianmeans93@gmail.com>
2025-11-06 07:21:18 -0500

    First real updates
    Added some comments for libraries and stuff to look into

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-'''Code goes here'''
-print('Hello world')
+'''For the main GUI, look into PyGame. TKinter may work but might be tedious and not very flashy.
+    Likely could stick to terminal during early development and polish it later.'''
+
+'''For video, look into OpenCV or MoviePy. Might be able to play video directly. If using PyGame pygvideo might work'''
+
+'''If using PyGame, it can handle game sounds'''
+
+'''Lights will be APA102 addressable LEDs using SPI. Check ECET 434 textbook ch.8 for example using Luma'''
+
+'''For communication with the PLC, look into pycomm3 or pylogix'''
+
+'''Scoring will be coded from scratch'''
