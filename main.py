75649974718e059a3741ebe87cd558edf1cd8933
ArmoksHolyBeard <ianmeans93@gmail.com>
2026-02-03 11:30:54 -0500

    Integrate audio/video with main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import random
 from pycomm3 import LogixDriver
 from pinball_PLC import PinballPLC
+import pygvideo
 
 RED = (210, 50, 50)
 ORANGE = (230, 110, 50)
@@ -12,14 +13,12 @@ GREEN = (0, 225, 50)
 BLUE = (70, 130, 240)
 PURPLE = (200, 0, 210)
 
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 512
-
 class Score:
+    # Maybe move these game objects to their own file
     def __init__(self, surface):
         self.surface = surface
         self.font = pygame.font.Font(size=64)
-        self.location = [random.randint(0, 500), random.randint(0, 500)]
+        self.location = [surface.get_size()[0] // 2, surface.get_size()[1] - 100]
         self.color = BLUE
         self.points = 0
     
@@ -30,15 +29,16 @@ class Score:
     def addPoints(self, points: int):
         self.points += points
 
-# Initialize pygame
+# Initialize pygame and pygame sound mixer
 pygame.init()
+pygame.mixer.init()
 
 # Initialize the game timer and specify the frame rate
 gameTime = pygame.time.Clock()
-FPS = 4
+FPS = 30
 
-# Initialize the window
-displaysurface = pygame.display.set_mode((1024, 512))
+# Initialize the window to fullscreen
+displaysurface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption("Pinball Test")
 
 # Set up custom events
@@ -47,50 +47,60 @@ PLC_GET = pygame.USEREVENT + 1
 # Initialize display objects
 score = Score(displaysurface)
 
-# Set up PLC comms
-plc = PinballPLC()
-plc.start()
+# Initialize audio and video clips
+cheer = pygame.mixer.Sound("Test Functions\cheer.wav")
+
+video = pygvideo.Video("Test Functions\messi_score.mp4")
+video.set_size(displaysurface.get_size())
+video.prepare()
+
+# # Set up PLC comms
+# plc = PinballPLC()
+# plc.start()
 
 # Game Loop
-while True:
+running = True
+while running:
 
     # Check all pygame events
     for event in pygame.event.get():
 
         # Quit the game
         if event.type == QUIT:  
-            plc.end()
-            pygame.quit()
-            sys.exit()
+            running = False
         
         if event.type == pygame.KEYDOWN:
             keys = pygame.key.get_pressed()
-            if keys[K_g]:
-                score.addPoints(500)
-        
-        # 
-        if event.type == PLC_GET:
-            plc.read()
-            if plc.getTags() != 0:
-                score.addPoints(750)
+            if keys[K_j]:
+                score.addPoints(2500)
+                cheer.play()
+            if keys[K_f]:
+                score.addPoints(20000)
+                video.play()
+            if keys[K_ESCAPE]:
+                pygame.event.post(pygame.event.Event(QUIT))
 
-    
-    if plc.isReady():
-        pygame.event.post(pygame.event.Event(PLC_GET))
+        # if event.type == PLC_GET:
+        #     plc.read()
+        #     if plc.getTags() != 0:
+        #         score.addPoints(750)
+
+        # video.handle_event(event)
+
+    # # Post event when PLC comms returns with a value
+    # if plc.isReady():
+    #     pygame.event.post(pygame.event.Event(PLC_GET))
 
     displaysurface.fill((255, 255, 255))
     score.update()
 
-    pygame.display.update()
+    if video.is_play:
+        video.draw_and_update(displaysurface, (0, 0))
+    
+    pygame.display.flip()
     gameTime.tick(FPS)
 
-'''For the main GUI, look into PyGame. TKinter may work but might be tedious and not very flashy.
-    Likely could stick to terminal during early development and polish it later.'''
-
-'''For video, look into OpenCV or MoviePy. Might be able to play video directly. If using PyGame pygvideo might work'''
-
-'''If using PyGame, it can handle game sounds'''
-
-'''For communication with the PLC, look into pycomm3 or pylogix'''
-
-'''Scoring will be coded from scratch'''
+# Maybe move these cleanup tasks to a function
+# plc.end()
+pygame.quit()
+sys.exit()
