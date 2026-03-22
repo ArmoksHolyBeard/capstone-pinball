4428b4a0169187fdb078be915de32a1fa8901e59
ArmoksHolyBeard <ianmeans93@gmail.com>
2026-03-21 23:18:53 -0400

    More motor testing

--- pinball_goaliemotor.py
+++ pinball_goaliemotor.py
@@ -10,9 +10,10 @@ from queue import Queue
 import board
 import digitalio
 
-# Time delay between steps in ms
-STEP_DELAY = 0.1
-TRIGGER_PULSE_TIME = 0.1
+# Time delay between steps in s
+TRIGGER_PULSE_TIME = 0.000005
+FAST_STEP = 0.000010
+SLOW_STEP = 0.000020
 
 
 class MotorController:
@@ -27,8 +28,8 @@ class MotorController:
     step_pin = digitalio.DigitalInOut(board.D6)
     step_pin.switch_to_output()
 
-    enable  = digitalio.DigitalInOut(board.D16)
-    enable.switch_to_output()
+    disable  = digitalio.DigitalInOut(board.D16)
+    disable.switch_to_output()
 
     right_sensor = digitalio.DigitalInOut(board.D13)
     right_sensor.switch_to_input()
@@ -41,49 +42,51 @@ class MotorController:
         self.count = 0
         self.right_step_limit = 0
         self.left_step_left = 0
-        self.state = self.disable()
+        self.state = self.disable_motor()
         self.endpoints_set = False
 
     def step_once(self):
         self.step_pin.value = True
-        time.sleep(0.2)
+        time.sleep(TRIGGER_PULSE_TIME)
         self.step_pin.value = False
-        time.sleep(0.8)
+        time.sleep(TRIGGER_PULSE_TIME)
 
-    def setDirection(self, d):
-        if d == 'R':
+    def set_direction(self, direction: str):
+        """ Options are 'R' and 'L' """
+        if direction == 'R':
             self.dir_pin.value = True
-        if d == 'L':
+        if direction == 'L':
             self.dir_pin.value = False
 
     def index_motor(self):
-        self.enable.value = True
+        self.disable.value = True
 
         # Get the right side limit
-        self.setDirection('R')
+        self.set_direction('R')
         while not self.right_sensor.value:
             self.step_once()
             self.count += 1
-            time.sleep(STEP_DELAY)
+            time.sleep(FAST_STEP)
             yield
         self.right_step_limit = self.count
         
         # Get the left limit
-        self.setDirection('L')
+        self.set_direction('L')
         while not self.left_sensor.value:
             self.step_once()
             self.count -= 1
-            time.sleep(STEP_DELAY)
+            time.sleep(FAST_STEP)
             yield
         self.left_step_left = self.count
         self.endpoints_set = True
 
-    def disable(self):
-        self.enable.value = False
+    def disable_motor(self):
+        self.disable.value = True
         while True:
             yield
     
     def defend(self):
+        self.disable.value = False
         while True:
             yield
     
@@ -97,14 +100,25 @@ class MotorController:
                     case self.INDEX:
                         self.state = self.index_motor()
                     case self.PAUSE:
-                        self.state = self.disable()
+                        self.state = self.disable_motor()
                     case self.DEFEND if self.endpoints_set:
                         self.state = self.defend()
                     case _:
-                        self.state = self.disable()
+                        self.state = self.disable_motor()
             else:
                 next(self.state)
 
 
 if __name__ == "__main__":
-    pass
+    q = Queue()
+    motor = MotorController(q)
+    for i in range(4):
+        motor.set_direction('R')
+        for j in range(800):
+            motor.step_once()
+            time.sleep(SLOW_STEP)
+        motor.set_direction('L')
+        for k in range(400):
+            motor.step_once()
+            time.sleep(FAST_STEP)
+
