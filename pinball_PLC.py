189159574d25f331ac1ee47a600d0a850d600b15
ArmoksHolyBeard <ianmeans93@gmail.com>
2026-03-31 06:09:55 -0400

    Various PLC Updates
    Added toggle lock method and relevant command handling.
    Commands are now class constants.
    Expanded test code

--- pinball_PLC.py
+++ pinball_PLC.py
@@ -17,6 +17,8 @@ KICKBACK = "Program:MainProgram.KBC.ACC"
 LIVES = "Program:MainProgram.Life_Counter.ACC"
 RAMP_SPINNER = "Program:MainProgram.RampC.ACC"
 STANDING_TARGETS = "Program:MainProgram.STC.ACC"
+# TODO: Tags for system enable, start button, maybe flippers
+SYSTEM_ENABLE = "foo"
 
 # Create list of tags
 allTags = [
@@ -45,11 +47,15 @@ tagNames = {
 
 class PinballPLC():
 
+    QUIT = 0
+    LOCK = 1
+    UNLOCK = 2
+
     def __init__(self, data_q: Queue, cmd_q: Queue, /, *, demo_mode=False):
         self.data_q = data_q
         self.cmd_q = cmd_q
         self.plc = PLC(IP) if not demo_mode else None
-        self.tagValues = {
+        self.tag_values = {
             'bumper_1': 0,
             'bumper_2': 0,
             'bumper_3': 0,
@@ -67,33 +73,49 @@ class PinballPLC():
             self.plc.Close()
     
     def _read(self):
-        if not self.demo_mode:
-            current_tags = self.plc.Read(allTags)
-            for tag in current_tags:
-                self.tagValues[tagNames[tag.TagName]] = tag.Value
-        return self.tagValues
+        current_tags = self.plc.Read(allTags)
+        for tag in current_tags:
+            self.tag_values[tagNames[tag.TagName]] = tag.Value
+        return self.tag_values
+    
+    def _toggle_lock(self, enabled: bool):
+        response = self.plc.Write(SYSTEM_ENABLE, enabled)
     
     def _resetTags(self):
-        if not self.demo_mode:
-            request = [(tag, 0) for tag in allTags]
-            response = self.plc.Write(request)
-            # TODO: Error checking on response
+        request = [(tag, 0) for tag in allTags]
+        response = self.plc.Write(request)
+        # TODO: Error checking on response
     
     def read_loop(self):
         # TODO: Error handling and exit condition
         while True:
             # Check the command queue
             if not self.cmd_q.empty():
-                if self.cmd_q.get() == "quit":
+                cmd = self.cmd_q.get()
+                if cmd == self.QUIT: # Maybe set a timeout with exception handling?
+                    # Turn off the motor driver and return so the
+                    # thread manager can properly close out the thread
                     self._end()
                     return
-            sleep(0.1)
+                if cmd == self.LOCK:
+                    self._toggle_enable(True)
+                if cmd == self.UNLOCK:
+                    self._toggle_enable(False)
             if not self.demo_mode:
-                self.data_q.put(self._read())
-            self._resetTags()
+                self.data_q.put(self._read()) # Maybe set a timeout with exception handling?
+                self._resetTags()
 
 if __name__ == "__main__":
-    testPLC = PinballPLC()
-    print(testPLC._read())
-    testPLC._resetTags()
-    testPLC._end()
+    test_Q1 = Queue()
+    test_Q2 = Queue()
+    test_PLC = PinballPLC(test_Q1, test_Q2)
+    print(test_PLC._read())
+    print(test_PLC._resetTags())
+    print()
+    if (input("Continue? y/n ")).upper() == "Y":
+        for i in range(20):
+            for tag, value in test_PLC._read().items():
+                print(f"{tag}: {value}")
+            print()
+            sleep(0.5)
+    test_PLC._end()
